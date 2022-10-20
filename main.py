3f17cc46a7110a4e3657a473a6463c717ad818d4
jin <jchris1227@icloud.com>
2022-10-20 01:59:07 -0700

    changes from dev-jin loc pc

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
-#1. Create a greeting for your program.
+# 1. Create a greeting for your program.
 print("Thank you for using the Band Name Generator!")
-#2. Ask the user for the city that they grew up in.
-user_city = input("Which city did you grew up in?\n")
-#3. Ask the user for the name of a pet.
-user_pet = input("What is the name of your pet?\n")
-#4. Combine the name of their city and pet and show them their band name.
-new_band_name = user_city +" "+ user_pet
-#5. Make sure the input cursor shows on a new line, see the example at:
+# 2. Ask the user for the city that they grew up in.
+# user_city = input("Which city did you grew up in?\n")
+user_city = "Kyoto"
+# 3. Ask the user for the name of a pet.
+# user_pet = input("What is the name of your pet?\n")
+user_pet = "Wee"
+# 4. Combine the name of their city and pet and show them their band name.
+new_band_name = user_city + " " + user_pet
+# 5. Make sure the input cursor shows on a new line, see the example at:
 print("Congratulations! Your New Band Name is " + new_band_name + "!!!")
 #   https://replit.com/@appbrewery/band-name-generator-end
+
+print("Change from dev-jin/main")
